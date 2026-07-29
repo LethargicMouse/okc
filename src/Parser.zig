26f3b5c5a2633740e40f32fdfd492b5fd48bb128
George Kozirev <gkozirev.0@gmail.com>
2026-07-29 23:34:36 +0300

    parse error msgs

--- src/Parser.zig
+++ src/Parser.zig
@@ -54,8 +54,8 @@ pub fn run(parser: *Parser) !Ast {
 }
 
 fn parse_ast(parser: *Parser) !Ast {
-    const funs = try parser.parse_many(Ast.Fun, parse_fun);
-    try parser.expect(.eof);
+    const funs = try parser.parse_many(Ast.Fun, parse_fun_loud);
+    try parser.expect_loud(.eof);
     return .{ .funs = funs };
 }
 
@@ -79,39 +79,52 @@ fn parse_maybe(parser: *Parser, typ: type, parse: fn (*Parser) ParseError!typ) !
     return res;
 }
 
-fn parse_fun(parser: *Parser) !Ast.Fun {
-    try parser.expect(.fun);
-    const name = try parser.parse_name();
-    try parser.expect(.parl);
-    try parser.expect(.parr);
-    try parser.expect(.{ .name = "i32" });
-    try parser.expect(.curl);
-    const statements = try parser.parse_many(Ast.Statement, parse_statement);
-    try parser.expect(.semi);
-    try parser.expect(.curr);
+fn parse_fun_loud(parser: *Parser) !Ast.Fun {
+    try parser.expect_loud(.fun);
+    const name = try parser.parse_name_loud();
+    try parser.expect_loud(.parl);
+    try parser.expect_loud(.parr);
+    try parser.expect_loud(.{ .name = "i32" });
+    try parser.expect_loud(.curl);
+    const statements = try parser.parse_many(Ast.Statement, parse_statement_loud);
+    try parser.expect_loud(.curr);
     return .{
         .name = name,
         .statements = statements,
     };
 }
 
-fn parse_statement(parser: *Parser) !Ast.Statement {
-    return parser.parse_ret_statement();
+fn parse_statement_loud(parser: *Parser) !Ast.Statement {
+    return parser.parse_ret_statement() catch |err| {
+        try parser.fail("<statement>");
+        return err;
+    };
 }
 
 fn parse_ret_statement(parser: *Parser) !Ast.Statement {
     try parser.expect(.ret);
-    const expr = try parser.parse_expr();
+    const expr = try parser.parse_expr_loud();
+    try parser.expect_loud(.semi);
     return .{ .ret = expr };
 }
 
-fn parse_expr(parser: *Parser) !Ast.Expr {
-    return parser.parse_int_expr();
+fn parse_expr_loud(parser: *Parser) !Ast.Expr {
+    return parser.parse_int_expr() catch |err| {
+        try parser.fail("<expr>");
+        return err;
+    };
 }
 
 fn parse_int_expr(parser: *Parser) !Ast.Expr {
     const int = try parser.parse_int();
     return .{ .int = int };
+}
+
+fn parse_name_loud(parser: *Parser) ![]const u8 {
+    return parser.parse_name() catch |err| {
+        try parser.fail("<name>");
+        return err;
+    };
 }
 
 fn parse_name(parser: *Parser) ![]const u8 {
@@ -141,6 +154,13 @@ fn parse_int(parser: *Parser) ![]const u8 {
 fn deinit(parser: *Parser) void {
     parser.gpa.free(parser.tokens);
     parser.err_msgs.deinit(parser.gpa);
+}
+
+fn expect_loud(parser: *Parser, lexeme: Lexeme) !void {
+    parser.expect(lexeme) catch |err| {
+        try parser.fail(lexeme.describe());
+        return err;
+    };
 }
 
 fn expect(parser: *Parser, lexeme: Lexeme) !void {
