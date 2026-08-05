6828397cc0135858c6ee0e57ff849eda2269a7d7
George Kozirev <gkozirev.0@gmail.com>
2026-08-05 20:21:38 +0300

    multiline location

--- src/Location.zig
+++ src/Location.zig
@@ -14,17 +14,23 @@ pub fn format(location: Location, writer: *std.Io.Writer) std.Io.Writer.Error!vo
         \\`{s}` at {f}:
         \\     |
     , .{ location.name, location.start });
-    // for now we only deal with one-liners
-    std.debug.assert(location.start.line == location.end.line);
     try line(writer, location.start.line, location.lines);
-    try underline(writer, location.start.symbol, location.end.symbol);
+    if (location.start.line == location.end.line) {
+        try underline(writer, location.start.symbol, location.end.symbol);
+        return;
+    }
+    try underline(writer, location.start.symbol, location.lines[location.start.line - 1].len);
+    for (location.start.line + 1..location.end.line + 1) |i| {
+        try line(writer, i, location.lines);
+    }
+    try underline(writer, 0, location.end.symbol);
 }
 
-fn line(writer: *std.Io.Writer, number: u32, lines: []const []const u8) std.Io.Writer.Error!void {
+fn line(writer: *std.Io.Writer, number: usize, lines: []const []const u8) std.Io.Writer.Error!void {
     try writer.print("\n{:>4} | {s}", .{ number, lines[number - 1] });
 }
 
-fn underline(writer: *std.Io.Writer, start: u32, end: u32) std.Io.Writer.Error!void {
+fn underline(writer: *std.Io.Writer, start: usize, end: usize) std.Io.Writer.Error!void {
     try writer.writeAll("\n     |");
     for (0..start) |_| {
         try writer.writeByte(' ');
